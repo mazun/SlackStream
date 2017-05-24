034ec21ec15f1ffd8fb143fa678499bb7ed63db4
y_mazun <y.mazun@gmail.com>
2017-05-24 23:50:51 +0900

    apply router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
+// angulars
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
 
 // components
 import { AppComponent } from './app.component';
 import { ChannelNameComponent } from '../components/slack/channel/name.component';
 import { MessageFormComponent } from '../components/slack/form/message.component';
+import { SlackListComponent } from '../components/slack/list/slacklist.component';
 
 // pipes
 import { ChannelColorPipe, TimeStampPipe } from '../pipes/slack';
@@ -15,16 +18,22 @@ import { SafeHtmlPipe, SafeURLPipe } from '../pipes/dom';
 import { SlackServiceCollection } from '../services/slack/slack.service';
 import { SettingService } from '../services/setting.service';
 
+const appRoutes: Routes = [
+  { path: '', component: SlackListComponent },
+];
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     // components
     AppComponent,
     ChannelNameComponent,
     MessageFormComponent,
+    SlackListComponent,
 
     // pipes
     ChannelColorPipe,
---
+++ src/components/slack/list/slacklist.component.ts
@@ -0,0 +1,149 @@
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
+import { SlackServiceCollection, SlackMessage, SlackService } from '../../../services/slack/slack.service';
+import { SlackParser, ComposedParser, LinkParser, EmojiParser } from '../../../services/slack/slack-parser.service';
+
+class DisplaySlackMessageInfo {
+    edited: boolean;
+
+    constructor(
+        public message: SlackMessage,
+        public parser: SlackParser,
+        public client: SlackService
+    ) {
+
+    }
+
+    get text(): string {
+        return this.parser.parse(this.message.text, this.message.dataStore);
+    }
+}
+
+interface SubmitContext {
+    channelName: string;
+    channelID: string;
+    teamID: string;
+
+    submit(text: string): Promise<any>;
+}
+
+class PostMessageContext implements SubmitContext {
+    constructor(
+        public client: SlackService,
+        public channelName: string,
+        public channelID: string,
+        public teamID: string
+    ) {
+    }
+
+    async submit(text: string): Promise<any> {
+        return this.client.postMessage(this.channelID, text);
+    }
+}
+
+@Component({
+  selector: 'ss-list',
+  templateUrl: './slacklist.component.html',
+  styles: [ require('./slacklist.component.css').toString() ],
+})
+export class SlackListComponent implements OnInit {
+  messages: DisplaySlackMessageInfo[] = [];
+  slackServices: SlackService[];
+  submitContext: SubmitContext = null;
+
+  get doesHaveMultipleTeams(): boolean {
+    return this.slackServices.length >= 2;
+  }
+
+  get showTeamName(): boolean {
+    return false;
+  }
+
+  constructor(
+    private services: SlackServiceCollection,
+    private detector: ChangeDetectorRef
+  ) {
+    this.slackServices = services.slacks;
+  }
+
+  ngOnInit(): void {
+    for (const slack of this.slackServices) {
+        const parser = new ComposedParser([
+            new LinkParser (),
+            new EmojiParser (slack)
+        ]);
+        slack.start();
+        slack.messages.subscribe(message => this.onReceiveMessage(message, parser, slack));
+    }
+    console.log('ready');
+  }
+
+  async onReceiveMessage(message: SlackMessage, parser: SlackParser, client: SlackService): Promise<void> {
+    console.log(message.rawMessage);
+
+    switch (message.rawMessage.subtype) {
+      case 'message_deleted':
+        await this.deleteMessage(message, parser, client);
+        break;
+      case 'message_changed':
+        await this.changeMessage(message, parser, client);
+        break;
+      case 'message_replied':
+        await this.replyMessage(message, parser, client);
+        break;
+      default:
+        await this.addMessage(message, parser, client);
+        break;
+    }
+
+    this.detector.detectChanges();
+  }
+
+  async addMessage(message: SlackMessage, parser: SlackParser, client: SlackService): Promise<void> {
+    if (message.message) {
+        this.messages.unshift(new DisplaySlackMessageInfo(message, parser, client));
+    }
+  }
+
+  async replyMessage(message: SlackMessage, parser: SlackParser, client: SlackService): Promise<void> {
+      // TODO
+  }
+
+  async deleteMessage(message: SlackMessage, parser: SlackParser, client: SlackService): Promise<void> {
+    this.messages = this.messages.filter (m => message.rawMessage.deleted_ts !== m.message.rawMessage.ts);
+  }
+
+  async changeMessage(message: SlackMessage, parser: SlackParser, client: SlackService): Promise<void> {
+    const edited = this.messages.find(m => m.message.rawMessage.ts === message.rawMessage.message.ts);
+    if (edited) {
+        edited.edited = true;
+        edited.message.text = message.rawMessage.message.text;
+    }
+  }
+
+  get showForm(): boolean {
+      return this.submitContext != null;
+  }
+
+  onClickWrite(info: DisplaySlackMessageInfo) {
+      this.submitContext = new PostMessageContext(
+          info.client,
+          info.message.channelName,
+          info.message.channelID,
+          info.message.teamID
+      );
+      this.detector.detectChanges();
+  }
+
+  async submitForm(text: string) {
+      if (this.submitContext != null) {
+          await this.submitContext.submit (text);
+          this.submitContext = null;
+          this.detector.detectChanges();
+      }
+  }
+
+  closeForm() {
+      this.submitContext = null;
+      this.detector.detectChanges();
+  }
+}
