0e6b996fe3d9f05cf08701075d704e2fa49b644d
y_mazun <y.mazun@gmail.com>
2017-05-30 23:23:32 +0900

    remove redundant code

--- src/services/slack/wrapper/webwrapper.ts
+++ src/services/slack/wrapper/webwrapper.ts
@@ -9,109 +9,65 @@ export class WebClientWrapper {
 
     async postMessage(channel: string, text: string): Promise<any> {
         return new Promise<any>((resolve, reject) => {
-            this.client.chat.postMessage(channel, text, { 'as_user': true, 'link_names': 1 }, (err, info) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(info);
-                }
-            });
+            this.client.chat.postMessage(channel, text, { 'as_user': true, 'link_names': 1 }, this.handler<any>(resolve, reject));
         });
     }
 
     async updateMessage(ts: string, channel: string, text: string): Promise<any> {
         return new Promise<any>((resolve, reject) => {
-            this.client.chat.update(ts, channel, text, { 'as_user': true, 'link_names': 1 }, (err, info) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(info);
-                }
-            });
+            this.client.chat.update(ts, channel, text, { 'as_user': true, 'link_names': 1 }, this.handler<any>(resolve, reject));
         });
     }
 
-    async deleteMessage(channel: string, timestamp: string) {
-        return new Promise<void>((resolve, reject) => {
-            this.client.chat.delete(timestamp, channel, { 'as_user': true }, (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
+    async deleteMessage(channel: string, timestamp: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.client.chat.delete(timestamp, channel, { 'as_user': true }, this.handler<any>(resolve, reject));
         });
     }
 
     async getEmoji(): Promise<{ string: string }> {
         return new Promise<{ string: string }>((resolve, reject) => {
-            this.client.emoji.list((err, info) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(info.emoji as { string: string });
-                }
-            });
+            this.client.emoji.list((err, info) => this.handler<{ string: string }>(resolve, reject));
         });
     }
 
-    async markRead(channel: string, timestamp: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this.client.channels.mark(channel, timestamp, (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
+    async markRead(channel: string, timestamp: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.client.channels.mark(channel, timestamp, this.handler<any>(resolve, reject));
         });
     }
 
-    async markReadDM(channel: string, timestamp: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this.client.im.mark(channel, timestamp, (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
+    async markReadDM(channel: string, timestamp: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.client.im.mark(channel, timestamp, this.handler<any>(resolve, reject));
         });
     }
 
-    async markReadGroup(channel: string, timestamp: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this.client.groups.mark(channel, timestamp, (err) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
+    async markReadGroup(channel: string, timestamp: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.client.groups.mark(channel, timestamp, this.handler<any>(resolve, reject));
         });
     }
 
-    async addReaction(reaction: string, channel: string, ts: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this.client.reactions.add(reaction, { 'timestamp': ts, 'channel': channel }, (err, info) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
+    async addReaction(reaction: string, channel: string, ts: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.client.reactions.add(reaction, { 'timestamp': ts, 'channel': channel }, this.handler<any>(resolve, reject));
         });
     }
 
-    async removeReaction(reaction: string, channel: string, ts: string): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            this.client.reactions.remove(reaction, { 'timestamp': ts, 'channel': channel }, (err, info) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve();
-                }
-            });
+    async removeReaction(reaction: string, channel: string, ts: string): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            this.client.reactions.remove(reaction, { 'timestamp': ts, 'channel': channel }, this.handler<any>(resolve, reject));
         });
+    }
+
+    handler<T>(resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void): ((err: any, info: any) => void) {
+        return (err, info) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(info as T);
+            }
+        }
     }
 }
