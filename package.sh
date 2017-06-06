ca77eb3f15e5b97813e8b84f0a8bab2dfceabf0b
y_mazun <y.mazun@gmail.com>
2017-06-07 00:43:57 +0900

    remove log of zip command when packaging

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@ cp -r dist electron package.json package
 electron-packager package aslack-stream --platform=all --arch=all --overwrite
 for f in aslack-stream*
 do
-    zip $f.zip -r $f
+    zip $f.zip -r $f > /dev/null
 done
 
 rm -rf package
