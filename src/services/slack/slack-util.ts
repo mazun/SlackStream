970b933ba87276376c3df415447f0681fe48a91e
y_mazun <y.mazun@gmail.com>
2017-06-14 20:39:41 +0900

    Parse channel without channel name like <#ABCDEFG> correctly, and use common code to restore original plain text in edit mode.

--- src/services/slack/slack-util.ts
+++ src/services/slack/slack-util.ts
@@ -1,6 +1,15 @@
 import { DataStore } from './slack.types';
 import * as seedrandom from 'seedrandom';
 
+export class ParseLinkResult {
+    get withLink(): string {
+        return this._withLink ? this._withLink : this.text;
+    }
+
+    constructor(public text: string, public _withLink: string = undefined) {
+    }
+}
+
 export class SlackUtil {
     static channelColor(channelName: string): string {
         let res = '#';
@@ -42,5 +51,57 @@ export class SlackUtil {
         }
 
         return '???';
+    }
+
+    static parseLink(text: string, dataStore: DataStore): ParseLinkResult {
+        text = text.substr(1, text.length - 2);
+        const bar = text.indexOf('|');
+        if (bar >= 0) {
+            const text1 = text.substr(0, bar);
+            const text2 = text.substr(bar + 1);
+            return this.parseLink2(text1, text2, dataStore);
+        } else {
+            return this.parseLink1(text, dataStore);
+        }
+    }
+
+    static parseLink1(text: string, dataStore: DataStore): ParseLinkResult {
+        if (text[0] === '@' || text[0] === '!') {
+            const user = dataStore.getUserById(text.substr(1));
+            if (user) {
+                return new ParseLinkResult(`@${user.name}`);
+            }
+            const bot = dataStore.getBotById(text.substr(1));
+            if (bot) {
+                return new ParseLinkResult(`@${bot.name}`);
+            }
+            return new ParseLinkResult('@' + text.substr(1));
+        } else if (text[0] === '#') {
+            const channel = dataStore.getChannelById(text[0].substr(1));
+            const color = SlackUtil.channelColor(channel.name);
+            // <ss-channelname> does not work...
+            const withLink = `<span class="channel-name" style="color: ${color};">#${channel.name}</span>`;
+            return new ParseLinkResult('#' + channel.name, withLink);
+        } else {
+            return new ParseLinkResult(text);
+        }
+    }
+
+    static parseLink2(text1: string, text2: string, dataStore: DataStore): ParseLinkResult {
+        if (text1[0] === '#') {
+            const channel = dataStore.getChannelById(text1.substr(1));
+            const color = SlackUtil.channelColor(channel.name);
+            // <ss-channelname> does not work...
+            const withLink = `<span class="channel-name" style="color: ${color};">#${text2}</span>`;
+            return new ParseLinkResult('#' + text2, withLink);
+        } else if (text1[0] === '@' || text1[0] === '!') {
+            if (text2[0] === '@') {
+                return new ParseLinkResult(text2);
+            } else {
+                return new ParseLinkResult(`@${text2}`);
+            }
+        } else {
+            return new ParseLinkResult(text2, `<a href="${text1}">${text2}</a>`);
+        }
     }
 }
