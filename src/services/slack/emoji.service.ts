2c4cba071f98a83e05f2d84817832472918ed975
Soramichi Akiyama <akiyama@m.soramichi.jp>
2017-07-07 19:10:01 +0900

    Show frequently used emojis first

--- src/services/slack/emoji.service.ts
+++ src/services/slack/emoji.service.ts
@@ -1,19 +1,33 @@
 import { defaultEmojis } from './default_emoji';
 import { SlackClient } from './slack-client';
-
+import * as fs from 'fs';
+import * as path from 'path';
 import * as emojione from 'emojione';
 import * as $ from 'jquery';
+
+let usedFrequencyPath = '';
+export function setUsedFrequencyPath(path: string) {
+    usedFrequencyPath = path;
+}
 
 export class EmojiService {
     emojiList: { string: string };
     defaultEmojis = defaultEmojis;
+    usedFrequency: {};
+    frequencyFile: string;
 
     get allEmojis(): string[] {
         return defaultEmojis.concat(Object.keys(this.emojiList));
     }
 
     constructor(private client: SlackClient) {
+        this.frequencyFile = path.join(usedFrequencyPath, this.client.token + '.feq.json');
         this.initExternalEmojis();
+        try {
+            this.usedFrequency = JSON.parse(fs.readFileSync(this.frequencyFile, 'utf8'));
+        } catch (e) {
+            this.usedFrequency = {};
+        }
     }
 
     async initExternalEmojis(): Promise<void> {
@@ -21,6 +35,15 @@ export class EmojiService {
             this.emojiList = await this.client.getEmoji();
         }
     }
+
+    useEmoji(emoji: string) {
+        if (!this.usedFrequency[emoji]) {
+            this.usedFrequency[emoji] = 1;
+        } else {
+            this.usedFrequency[emoji]++;
+        }
+        fs.writeFile(this.frequencyFile, JSON.stringify(this.usedFrequency), () => {}); // async
+    };
 
     convertEmoji(emoji: string, withTitle = true, skinTone = 0): string {
         if (this.emojiList && !!this.emojiList[emoji.substr(1, emoji.length - 2)]) {
